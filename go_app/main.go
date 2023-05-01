0914a53d727104e3582121792d091f04d35aa0bb
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-05-01 21:35:14 +0000

    Made random number generator in go

---
+++ go_app/main.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2023 Mohamad All rights reserved
+//
+// Created by: Mohamad
+// Created on: May 1
+
+// This program generates a number from 6 to -6 (excluding 0)
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Declare variables
+	var randomNumber float64
+	var answer string
+
+	// Ask user for negative or positive number
+	fmt.Println("What type of number do you want to generate? (n) negative or (p) positive?")
+	fmt.Scanln(&answer) // get user input
+
+	// Generate random seed for random number
+	rand.Seed(time.Now().UnixNano())
+
+	// Check which answer the user has chosen
+	if (answer == "p") {
+		randomNumber = float64(rand.Intn(6-1)+1)
+		fmt.Println("\nYour random number is: ", randomNumber)
+	} else if (answer == "n") {
+		randomNumber = float64(rand.Intn(6-1)+1) * -1
+		fmt.Println("\nYour random number is: ", randomNumber)
+	} else {
+		fmt.Println("\nInvalid input")
+	}
+
+	fmt.Println("\nDone.")
+}
